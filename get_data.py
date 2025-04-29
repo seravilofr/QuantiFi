1feb0a5e1b3d4eaf7d0a6cde004c1c87b15d202d
François Olivares <156725976+seravilofr@users.noreply.github.com>
2025-04-29 12:55:46 +0000

    create Flask library

--- get_data.py
+++ get_data.py
@@ -1,24 +1,29 @@
 import yfinance as yf
 
-def get_index_value(symbol):
-    ticker = yf.Ticker(symbol)
-    data = ticker.history(period="1d")
-    if not data.empty:
-        return round(data['Close'].iloc[-1], 2)
-    else:
-        return None
-
-if __name__ == "__main__":
+def get_indices_values():
     indices = {
         "S&P 500": "^GSPC",
         "CAC 40": "^FCHI",
         "DAX": "^GDAXI"
     }
 
+    results = {}
+
     for name, symbol in indices.items():
-        value = get_index_value(symbol)
-        if value:
-            print(f"{name}: {value}")
+        ticker = yf.Ticker(symbol)
+        data = ticker.history(period="1d")
+        if not data.empty:
+            price = round(data['Close'].iloc[-1], 2)
+            results[name] = price
+        else:
+            results[name] = None  # En cas de problème
+
+    return results
+
+if __name__ == "__main__":
+    values = get_indices_values()
+    for name, price in values.items():
+        if price:
+            print(f"{name}: {price}")
         else:
             print(f"{name}: données non disponibles")
-
